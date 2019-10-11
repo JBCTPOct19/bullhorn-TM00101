4bc808621fb2d85f7359fa7dbca9672a79c4c1d7
TPCF235016UR <TPCF235016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-11 17:56:50 -0400

    BullHorn

---
+++ src/main/java/com/example/demo/MESSAGE.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MESSAGE {
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private long id;
+
+        @NotNull
+        private String content;
+
+        @NotNull
+        private int date;
+
+        @NotNull
+        private String sentby;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
